2c38b9688352b703715e5e28b6198ce68ca9d512
Jovana <jovanablagojevic98@gmail.com>
2023-10-12 00:14:20 +0200

    little update

---
+++ prisma/script.ts
@@ -0,0 +1,22 @@
+/**
+ * Fetch data from the coruse Table
+ * Insert  the data intp Course Table
+ *
+ *
+ */
+
+import { PrismaClient } from "@prisma/client";
+
+const prismaClient = new PrismaClient();
+
+async function main() {
+  data: {
+  }
+
+  //write queires here
+  //Insert the record into course table
+  //we need a course model
+  prisma.course.create({});
+  //insert the record into course like title, desc, duration
+}
+main();
