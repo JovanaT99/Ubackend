2655d28758c93f37e0093d1aec6b94ab63306f2c
Jovana <jovanablagojevic98@gmail.com>
2023-10-26 23:36:49 +0200

    Continue

--- src/server.ts
+++ src/server.ts
@@ -14,14 +14,14 @@ const prisma = new PrismaClient();
 //apply third part middleware
 
 app.use(morgan("dev"));
+app.use(express.json()); //parse req body
 
-app.get("/", (req, res) => {
+app.get("/", (_req, _res) => {
   throw new Error("NEW ERROR");
-  res.send("Hello world");
 });
 
 //GET api/course
-app.get("/course", async (req, res) => {
+app.get("/course", async (_req, res) => {
   //fetch all the courses from db
   //create a new instructor
 
@@ -35,31 +35,17 @@ app.get("/course", async (req, res) => {
   return res.json(courses);
 });
 //create endpoint
-app.post("/api/course", async (req, res) => {
-  //create new record
+app.post("/api/course", async (_req, res) => {
+  //validate our parameters
 
-  // const instructor = await prisma.instructor.create({
-  //   data: {
-  //     name: "Jovana",
-  //     city: "London",
-  //     country: "UK",
-  //     email: "jovana@gmail.com",
-  //     zip: "2300",
-  //   },
-  // });
-
+  //validate title, duration, des and instructorId
   const course = await prisma.course.create({
     //add manually
     data: {
-      title: "Learn Sails.js",
-      desc: "Build apis with Sailsjs",
-      duration: 12.4,
-      instructorId: 1,
-      // Instructor: {
-      //   connect: {
-      //     id: 1,
-      //   },
-      // },
+      title: _req.body.title,
+      desc: _req.body.desc,
+      duration: _req.body.duration,
+      instructorId: _req.body.instructorId,
     },
   });
   //send the record back to resposnse
@@ -70,8 +56,8 @@ app.post("/api/course", async (req, res) => {
 app.use(
   (
     error: any,
-    req: express.Request,
-    res: express.Response,
+    _req: express.Request,
+    _res: express.Response,
     next: express.NextFunction
   ) => {
     console.log(`ERROR: ${error.message}`);
@@ -85,6 +71,8 @@ app.use(
 
 //manually input here
 
-app.use(errorHandler());
+if (process.env.NODE_ENV === "development ") {
+  app.use(errorHandler);
+}
 
 export default app;
